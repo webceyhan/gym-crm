58a8373dc4b430ff747ce43fae40b3d1b206115a
Evren Ceyhan <webceyhan@ceymac.lan>
2020-07-29 23:18:30 +0200

    members table created

---
+++ database/migrations/2020_07_29_203257_create_members_tabe.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTabe extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('nin')->unique();
+            $table->date('birth_date');
+            $table->string('birth_place')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('address')->nullable();
+            $table->string('photo')->nullable();
+            $table->text('notes')->nullable();
+            $table->enum('status', ['in', 'out', 'suspended'])->default('out');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
