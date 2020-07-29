509aeee96b8918d9d71024c4664548661f0eae11
Evren Ceyhan <webceyhan@ceymac.lan>
2020-07-29 22:31:08 +0200

    plans table created

---
+++ database/migrations/2020_07_29_200418_create_plans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->enum('type', ['daily', 'weekly', 'monthly', 'yearly', 'indefinite'])->default('indefinite');
+            $table->integer('duration')->default(0);
+            $table->decimal('price', 8, 2)->default(0);
+            $table->boolean('installment')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
