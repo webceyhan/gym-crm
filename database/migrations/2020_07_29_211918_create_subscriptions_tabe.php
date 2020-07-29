120dbda73adfa500adeee26951129c030a36d408
Evren Ceyhan <webceyhan@ceymac.lan>
2020-07-30 01:46:29 +0200

    subscriptions table created

---
+++ database/migrations/2020_07_29_211918_create_subscriptions_tabe.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTabe extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('plan_id')->constrained();
+            $table->foreignId('member_id')->constrained();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->decimal('balance', 8, 2)->default(0);
+            $table->timestamps();
+            $table->timestamp('cancelled_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
