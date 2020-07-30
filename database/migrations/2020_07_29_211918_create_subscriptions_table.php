c28ae9bd9da5e87a1138466fb653d91b77114107
Evren Ceyhan <webceyhan@ceymac.lan>
2020-07-30 02:10:42 +0200

    typo fixed in migration file names

--- database/migrations/2020_07_29_211918_create_subscriptions_table.php
+++ database/migrations/2020_07_29_211918_create_subscriptions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSubscriptionsTabe extends Migration
+class CreateSubscriptionsTable extends Migration
 {
     /**
      * Run the migrations.
