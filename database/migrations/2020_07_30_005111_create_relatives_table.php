44645453f2fa6bab9b0ace03138cc33a95c9149d
Evren Ceyhan <webceyhan@ceymac.lan>
2020-07-30 02:55:32 +0200

    relatives table created

---
+++ database/migrations/2020_07_30_005111_create_relatives_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRelativesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('relatives', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_id')->constrained();
+            $table->foreignId('related_id')->constrained('members');
+            $table->enum('type', ['sibling', 'family', 'friend'])->default('sibling');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('relatives');
+    }
+}
