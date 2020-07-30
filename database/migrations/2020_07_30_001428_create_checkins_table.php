f98f1201ec6154c5db48e604156cad6d68029e8c
Evren Ceyhan <webceyhan@ceymac.lan>
2020-07-30 02:41:11 +0200

    checkins table created

---
+++ database/migrations/2020_07_30_001428_create_checkins_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCheckinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkins', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subscription_id')->constrained();
+            $table->timestamps();
+            $table->timestamp('completed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checkins');
+    }
+}
