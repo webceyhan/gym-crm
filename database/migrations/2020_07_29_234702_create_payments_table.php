8e5924eaad6c4940efcd0c7302d5edeaecdd7872
Evren Ceyhan <webceyhan@ceymac.lan>
2020-07-30 02:13:28 +0200

    payments table created

---
+++ database/migrations/2020_07_29_234702_create_payments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subscription_id')->constrained();
+            $table->decimal('amount', 8, 2)->default(0);
+            $table->enum('method', ['cash', 'card', 'online'])->default('cash');
+            $table->enum('type', ['charge', 'refund', 'discount'])->default('charge');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
