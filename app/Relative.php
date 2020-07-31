a65873dc36995f0c71e5f35ef5edcb4914bab208
Evren Ceyhan <webceyhan@ceymac.lan>
2020-07-31 12:48:24 +0200

    relative scopes added

--- app/Relative.php
+++ app/Relative.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class Relative extends Pivot
@@ -20,4 +21,17 @@ class Relative extends Pivot
      */
     public $incrementing = true;
 
+    // SCOPES //////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Scope a query to only include relatives of given type.
+     *
+     * @param string type
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeOfType(Builder $query, string $type)
+    {
+        return $query->where('type', $type);
+    }
 }
