3a72238542e869ed3fff4a0751755482e6c7a39f
Evren Ceyhan <webceyhan@ceymac.lan>
2020-07-31 12:50:44 +0200

    plan scopes added

--- app/Plan.php
+++ app/Plan.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
@@ -21,6 +22,42 @@ class Plan extends Model
     public function payments()
     {
         return $this->hasManyThrough('App\Payment', 'App\Subscription');
+    }
+
+    // SCOPES //////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Scope a query to only include plans of given type.
+     *
+     * @param string $type
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeOfType(Builder $query, string $type)
+    {
+        return $query->where('type', $type);
+    }
+
+    /**
+     * Scope a query to only include prepaid plans.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopePrepaid(Builder $query)
+    {
+        return $query->where('installment', false);
+    }
+
+    /**
+     * Scope a query to only include installment plans.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeInstallment(Builder $query)
+    {
+        return $query->where('installment', true);
     }
 
 }
