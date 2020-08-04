5d7d3ee62a7c1ef41882385136d64d3e7b133d71
Evren Ceyhan <webceyhan@ceymac.lan>
2020-08-04 17:25:48 +0200

    subscription seeder refactored

--- database/seeds/SubscriptionSeeder.php
+++ database/seeds/SubscriptionSeeder.php
@@ -15,25 +15,43 @@ class SubscriptionSeeder extends Seeder
     public function run()
     {
         $planIds = Plan::pluck('id');
-        $config = config('seeder.member.subscription');
 
-        Member::all()->each(function ($member) use ($planIds, $config) {
+        Member::all()->each(function ($member) use ($planIds) {
 
+            // 0-3 subscriptions per member
+            $amount = rand(0, 3);
+
+            // start from member's creation
             $now = $member->created_at->clone();
 
-            $subscriptions = factory(Subscription::class, rand(...$config['count']))->make([
+            // make subscriptions
+            $subscriptions = factory(Subscription::class, $amount)->make([
                 'plan_id' => fn() => $planIds->random(),
-                'member_id' => $member->id,
             ]);
 
-            $subscriptions->each(function ($subscription) use ($now, $config) {
-                $subscription->created_at = $now->clone();
-                $subscription->start_date = $now->addDays(rand(...$config['delay_days']))->clone();
-                $subscription->end_date = $now->addMonths($subscription->plan->duration)->clone();
-                $subscription->cancelled_at = rand(0, 100) < 10 ? $now : null;
-                $subscription->balance = -$subscription->plan->price;
-                $subscription->save();
+            // customize subscriptions
+            $subscriptions->each(function ($subscription) use ($now) {
+                // get chosen plan
+                $plan = $subscription->plan;
+
+                // accredit balance with plan price (+ extra fees)
+                $subscription->balance = -($plan->price + $plan->extra_fee);
+
+                // schedule 0-2 weeks after previous one
+                $subscription->created_at = $now->addWeeks(rand(0, 2))->clone();
+
+                // delay 0-10 days after schdule
+                $subscription->start_date = $now->addDays(rand(0, 10))->clone();
+
+                // add months as long as the selected plan's duration
+                $subscription->end_date = $now->addMonths($plan->duration)->clone();
+
+                // optionally cancel and rewind now-date to 1-n months ago (max as long as plan's duration)
+                $subscription->cancelled_at = rand(0, 2) ? null : $now->subMonths(rand(1, $plan->duration))->clone();
             });
+
+            // save subscriptions
+            $member->subscriptions()->saveMany($subscriptions);
 
         });
     }
