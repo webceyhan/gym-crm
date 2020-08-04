68f0196b6e72126f00dcd595317f452b11a2939d
Evren Ceyhan <webceyhan@ceymac.lan>
2020-08-04 14:51:55 +0200

    member seeder refactored

--- database/seeds/MemberSeeder.php
+++ database/seeds/MemberSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Member;
+use App\Plan;
 use Illuminate\Database\Seeder;
 
 class MemberSeeder extends Seeder
@@ -12,11 +13,23 @@ class MemberSeeder extends Seeder
      */
     public function run()
     {
-        $config = config('seeder.member');
-        $now = config('seeder.now')->clone();
+        Plan::all()->each(function ($plan) {
 
-        factory(Member::class, $config['count'])->create([
-            'created_at' => fn() => $now->addDays(rand(0, 7)),
-        ]);
+            // 1-10 members per plan
+            $amount = rand(1, 10);
+
+            // start from plan's creation
+            $now = $plan->created_at->clone();
+
+            // make members
+            $members = factory(Member::class, $amount)->make();
+
+            // customize and save members
+            $members->each(function ($member) use ($now) {
+                $member->created_at = $now->addDays(rand(0, 7));
+                $member->verified_at = rand(0, 2) ? $now->addDays(rand(0, 10)) : null;
+                $member->save();
+            });
+        });
     }
 }
