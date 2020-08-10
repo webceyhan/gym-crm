cfb1916b170e815c060784d11f71588bb49ba4af
Evren Ceyhan <webceyhan@ceymac.lan>
2020-08-10 13:09:47 +0200

    member photo component added

---
+++ resources/js/components/MemberPhoto.vue
@@ -0,0 +1,28 @@
+<template>
+  <img :src="src" @error="fallback = true" />
+</template>
+
+<script>
+export default {
+  props: {
+    member: { type: Object },
+  },
+  data() {
+    return {
+      fallback: false,
+      avatar: {
+        male: "/img/avatar-male.jpg",
+        female: "/img/avatar-female.jpg",
+      },
+    };
+  },
+  computed: {
+    src() {
+      const { photo, gender } = this.member;
+      const avatar = this.avatar[gender || "male"];
+
+      return this.fallback ? avatar : photo || avatar;
+    },
+  },
+};
+</script>
